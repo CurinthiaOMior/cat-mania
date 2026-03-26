0fcf9832206b3afa60c6a9f7f631e4e112ee5499
JUAN SARAGOZA DE AMORIM <juan_amorim@estudante.sesisenai.org.br>
2026-03-26 10:10:39 -0300

    desenvolvimento das classes gato e cachorro

--- index.js
+++ index.js
@@ -1,54 +1,115 @@
 export const canvas = document.getElementById('canvas')
 export const contexto = canvas.getContext('2d')
+import Cachorro from "./models/Cachorro.js"
 import Gato from "./models/Gato.js"
 
-const POS_X_GATO = canvas.width / 2
-const POS_Y_GATO = canvas.height / 3
 const ALTURA_GATO = 100
 const LARGURA_GATO = 50
-const VELOCIDADE_GATO = 1
+const POS_X_GATO = canvas.width / 2 - LARGURA_GATO
+const POS_Y_GATO = canvas.height / 3 - ALTURA_GATO / 2
+const VELOCIDADE_GATO = 5
 
-const POS_X_GATO2 = (canvas.width / 2)
-const POS_Y_GATO2 = ((canvas.height / 3) * 2)
 const ALTURA_GATO2 = 100
 const LARGURA_GATO2 = 50
-const VELOCIDADE_GATO2 = 3
+const POS_X_GATO2 = canvas.width / 2 - LARGURA_GATO
+const POS_Y_GATO2 = canvas.height / 3 * 2
+const VELOCIDADE_GATO2 = 5
 
-let gato1 = new Gato(POS_X_GATO, POS_Y_GATO,  ALTURA_GATO, LARGURA_GATO, VELOCIDADE_GATO, './img/gato/gato1.png')
-let gato2 = new Gato(POS_X_GATO2, POS_Y_GATO2,  ALTURA_GATO2, LARGURA_GATO2, VELOCIDADE_GATO2, './img/gato/gato1.png')
+const gatos = [new Gato(POS_X_GATO, POS_Y_GATO, LARGURA_GATO, ALTURA_GATO, VELOCIDADE_GATO, './img/gato/gato1.png'), new Gato(POS_X_GATO2, POS_Y_GATO2, LARGURA_GATO2, ALTURA_GATO2, VELOCIDADE_GATO2, './img/gato/gato1.png')]
 
-document.addEventListener('keydown', (e) =>{
-if(e.key === 's'){
-    gato1.direcao = 1  
+const LARGURA_MIN_CACHORRO = 75
+const LARGURA_MAX_CACHORRO = 125
+const VELOCIDADE_MIN_CACHORRO = 5
+const VELOCIDADE_MAX_CACHORRO = 6
+const PROPORCAO_CACHORRO = 1.25
+
+const QUANTIDADE_CACHORROS = 10
+
+const cachorros = []
+for (let i = 0; i < QUANTIDADE_CACHORROS; i++) {
+    const LARGURA_CACHORRO = Math.random() * (LARGURA_MAX_CACHORRO - LARGURA_MIN_CACHORRO) + LARGURA_MIN_CACHORRO;
+    const ALTURA_CACHORRO = LARGURA_CACHORRO * PROPORCAO_CACHORRO;
+    const POS_X_CACHORRO = -LARGURA_MAX_CACHORRO - (Math.random() * canvas.width * i)
+    const POS_Y_CACHORRO = (-ALTURA_CACHORRO / 2) + Math.random() * ((canvas.height - ALTURA_CACHORRO / 2 - 1) - (-ALTURA_CACHORRO / 2));
+    const VELOCIDADE_CACHORRO = Math.random() * (VELOCIDADE_MAX_CACHORRO - VELOCIDADE_MIN_CACHORRO) + VELOCIDADE_MIN_CACHORRO;
+    cachorros.push(new Cachorro(POS_X_CACHORRO, POS_Y_CACHORRO, LARGURA_CACHORRO, ALTURA_CACHORRO, VELOCIDADE_CACHORRO, './img/cachorro/cachorro1.png'))
 }
-else if(e.key === 'w'){
-    gato1.direcao = -1
-}
-if(e.key === 'ArrowUp'){
-    gato2.direcao = -1
-}
-else if(e.key === 'ArrowDown'){
-    gato2.direcao = 1
-}
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 's') {
+        gatos[0].direcao = 1
+    }
+    if (e.key === 'w') {
+        gatos[0].direcao = -1
+    }
+    if (e.key === 'ArrowUp') {
+        gatos[1].direcao = -1
+    }
+    if (e.key === 'ArrowDown') {
+        gatos[1].direcao = 1
+    }
 });
 
-document.addEventListener('keyup', (e) =>{
-    gato1.direcao = 0
+document.addEventListener('keyup', (e) => {
+    if (e.key === 's' || e.key === 'w') {
+        gatos[0].direcao = 0
+    }
+    if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+        gatos[1].direcao = 0
+    }
 });
 
-function atualiza(){
-    gato1.atualiza()
-    gato2.atualiza()
+function resetarCachorros() {
+    cachorros.forEach(cachorro => {
+        if (cachorro.posX >= canvas.width + cachorro.largura) {
+            cachorro.reseta()
+        }
+    })
 }
 
-function renderiza(){
-    contexto.clearRect(0,0,canvas.width, canvas.height)
-    gato1.renderiza()
-    gato2.renderiza()
+function checarColisao() {
+    gatos.forEach(gato => {
+        cachorros.forEach(cachorro => {
+            if (gato.colideCom(cachorro)) {
+                cachorro.mata()
+                gato.vida = Math.max(0, gato.vida - 1) // Math.max retorna o menor numero dos 2
+                console.log(gato.vida)
+            }
+        })
+    })
 }
 
+function checarMorte() {
+    gatos.forEach(gato => {
+        if (gato.vida <= 0) {
+            gato.mata()
+        }
+    })
+}
 
-function main(){
+function atualiza() {
+    gatos.forEach(gato => {
+        gato.atualiza()
+    })
+    cachorros.forEach(cachorro => {
+        cachorro.atualiza()
+    })
+    checarMorte()
+    checarColisao()
+    resetarCachorros()
+}
+
+function renderiza() {
+    contexto.clearRect(0, 0, canvas.width, canvas.height)
+    gatos.forEach(gato => {
+        gato.renderiza()
+    })
+    cachorros.forEach(cachorro => {
+        cachorro.renderiza()
+    })
+}
+
+function main() {
     atualiza()
     renderiza()
     requestAnimationFrame(main)
--- models/Entidade.js
+++ models/Entidade.js
@@ -1,30 +1,44 @@
 import { contexto } from "../index.js"
 
-export default class Entidade{
-    constructor(posX,posY,altura,largura,velocidade,sprite){
+export default class Entidade {
+    constructor(posX, posY, altura, largura, velocidade, sprite) {
         this.posX = posX
         this.posY = posY
-        this.altura = altura
         this.largura = largura
+        this.altura = altura
         this.velocidade = velocidade
         this.sprite = new Image()
         this.sprite.src = sprite
     }
-    
-    renderiza(){
-        contexto.drawImage(this.sprite, this.posX, this.posY, this.altura, this.largura)
-        console.log(this)
+
+    renderiza() {
+        contexto.drawImage(this.sprite, this.posX, this.posY, this.largura, this.altura);
+
+        // pra tirar a hitbox tira da qui pra baixo
+        const centroX = this.posX + this.largura / 2;
+        const centroY = this.posY + this.altura / 2;
+        const raioX = (this.largura / 2) * 0.9;
+        const raioY = (this.altura / 2) * 0.9;
+
+        contexto.beginPath();
+        contexto.ellipse(centroX, centroY, raioX, raioY, 0, 0, Math.PI * 2);
+        contexto.strokeStyle = "red";
+        contexto.stroke();
     }
 
-    colisao(entidade){
-        if((this.x < entidade.x + entidade.w)&&
-        (this.x + this.w > entidade.x)&&
-        (this.y < entidade.y + entidade.h)&&
-        (this.y + this.h > entidade.y)){
-          return true
-      }else{
-          return false
-      }
+    colideCom(entidade) {
+        const centroX = this.posX + this.largura / 2;
+        const centroY = this.posY + this.altura / 2;
+        const centroXOutro = entidade.posX + entidade.largura / 2;
+        const centroYOutro = entidade.posY + entidade.altura / 2;
+        
+        const dx = centroX - centroXOutro;
+        const dy = centroY - centroYOutro;
+        const rx = (this.largura + entidade.largura) / 2 * 0.9;
+        const ry = (this.altura + entidade.altura) / 2 * 0.9;
+        
+        // verifica se a soma dos raios é menor que a distancia entre as entidades (paint)
+        //delta = d ; r = raio
+        return (dx / rx) ** 2 + (dy / ry) ** 2 <= 1;
     }
 }
-
